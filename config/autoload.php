4e545bb01cf1a44b8f1999174032228de990735f
MLD-pro <morgan.ld.pro@gmail.com>
2025-06-25 13:42:14 +0000

    fichier config

--- config/autoload.php
+++ config/autoload.php
@@ -1,5 +1,6 @@
 <?php 
 
 require_once "Router.php";
+require_once __DIR__ . '/../controllers/UserControllers.php';
 
 ?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'config/autoload.php';
+$router = new Router();
+$router->handleRequest($_GET);
+
+?>
