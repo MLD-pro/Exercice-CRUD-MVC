5bc5ccc087f9ab9da529a7557b76b872fdf0fcdd
MLD-pro <morgan.ld.pro@gmail.com>
2025-06-25 12:43:21 +0000

    dossier config

---
+++ config/Router.php
@@ -0,0 +1,56 @@
+<?php 
+
+require_once __DIR__ . '/../controllers/UserControllers.php';
+
+class Router {
+     public function __construct() {
+         
+     }
+     
+    public function handleRequest(array $get) : void {
+        $controller = new UserController();
+        
+        if (!isset($get["route"])) {
+               $controller->home();
+        }
+        
+        if (isset($get["route"])) {
+    
+            if ($get["route"] === "show_user") {
+                        
+                $controller->show();
+        
+        
+            } elseif ($get["route"] === "create_user") {
+                        
+                $controller->create();
+        
+        
+            } elseif ($get["route"] === "check_create_user") {
+                      
+                $controller->checkCreate();
+        
+        
+            } elseif ($get["route"] === "update_user") {
+                        
+                $controller->update();
+        
+        
+            } elseif ($get["route"] === "check_update_user") {
+                       
+                $controller->checkUpdate();
+        
+        
+            } elseif ($get["route"] === "delete_user") {
+                     
+                $controller->delete();
+        
+            } else {
+                       
+                $controller->list();
+            }
+        }
+    }
+}
+
+?>
