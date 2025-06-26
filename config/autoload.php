16e880d536ad53a56846c767c93a5cceb6245930
MLD-pro <morgan.ld.pro@gmail.com>
2025-06-26 09:07:34 +0000

    dossier exercice CRUD MVC

--- config/autoload.php
+++ config/autoload.php
@@ -3,5 +3,6 @@
 require_once "Router.php";
 require_once __DIR__ . '/../controllers/UserControllers.php';
 require_once __DIR__ . '/../models/User.php';
+require_once __DIR__ . '/../managers/AbstractManager.php';
 
 ?>
---
+++ managers/AbstractManager.php
@@ -0,0 +1,25 @@
+<?php 
+
+class AbstractManager {
+    protected PDO $db;
+            
+    public function __construct() {
+                
+            $host = "db.3wa.io";
+            $port = "3306";
+            $dbname = "morganledez_pooj1";
+            $connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+        
+            $user = "morganledez";
+            $password = "fddf083ac397e60104689ea7c5404f7c";
+        
+            $this->db = new PDO(
+            $connexionString,
+            $user,
+            $password
+        );
+    }
+}
+
+
+?>
