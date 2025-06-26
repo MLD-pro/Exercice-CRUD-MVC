ea70599a02489bce1fa6941e04d4c4593f3c3531
MLD-pro <morgan.ld.pro@gmail.com>
2025-06-26 08:11:46 +0000

    dossier exercice CRUD MVC

--- models/User.php
+++ models/User.php
@@ -1,7 +1,53 @@
-<?php 
+<?php
 
 class User {
+    private ?int $id = null;
+    private string $email;
+    private string $firstName;
+    private string $lastName;
+
     
+    public function __construct(string $email, string $firstName, string $lastName) {
+        $this->email = $email;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+
+
+    public function getId(): ?int {
+        return $this->id;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+    public function getFirstName(): string {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string {
+        return $this->lastName;
+    }
+
+    
+    
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function setEmail(string $email): void {
+        $this->email = $email;
+    }
+
+    public function setFirstName(string $firstName): void {
+        $this->firstName = $firstName;
+    }
+
+    public function setLastName(string $lastName): void {
+        $this->lastName = $lastName;
+    }
 }
 
 ?>
